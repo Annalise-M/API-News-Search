3cccd9425c8256af5d10d04044cd022632096d92
Annalise Murphy <nali247@gmail.com>
2020-10-25 19:29:58 -0700

    FINALLY PASSING FUCKING TESTINGGGGG with node-fetch import

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,5 +3,7 @@ import AllArticles from '../../containers/AllArticles.jsx';
 // import Loading from './loading/Loading.jsx';
 
 export default function App() {
+ 
+ 
   return <AllArticles />;
 }
--- src/services/article-api.jsx
+++ src/services/article-api.jsx
@@ -1,9 +1,8 @@
+const fetch = require('node-fetch');
+
 export const getArticles = () => {
   // eslint-disable-next-line max-len
-  return fetch('https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=33cf0171730d42829502b6a3698bfd81'
-    // { mode: 'no-cors'
-    // }
-  )
+  return fetch('https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=33cf0171730d42829502b6a3698bfd81')
     .then(res => res.json())
     .then(json => json.articles.map(article => ({
       title: article.title,
@@ -11,5 +10,3 @@ export const getArticles = () => {
       description: article.description
     })));
 };
-
-//https://cors-anywhere.herokuapp.com/
